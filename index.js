549a3bedfba01bbe0180b7f9f4de9872085860f5
CatsAreMLG <joshuadgon@gmail.com>
2019-03-09 14:37:10 -0600

    add index file

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express')
+const ProjectsRouter = require('./data/routers/projectsRouter.js')
+const ActionsRouter = require('./data/routers/actionsRouter.js')
+const server = express()
+server.use(express.json())
+// server.use('/api/posts', ProjectsRouter)
+// server.use('/api/users', ActionsRouter)
+const PORT = 8888
 
-// code away!
+server.get('/', (req, res) => {
+  res.send(`<h2>Welcome to the API</h2>`)
+})
+
+server.listen(PORT, _ =>
+  console.log(`SERVER RUNNING ON http://localhost:${PORT}`)
+)
